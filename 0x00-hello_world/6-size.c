98007557c5c9f640b31ea7da4cb62273167c1d77
Adej0la <recursr.dev@gmail.com>
2022-10-14 22:11:38 +0000

    Add C program that prints size of data types in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Prints the size of various data types in C
+  *
+  * Description: Prints the size of various data types on the computer it is
+  * compiled and run on
+  *
+  * Return: Always 0 (Successful)
+  */
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n",(int) sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n",(int) sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", (int) sizeof(float));
+	return (0);
+}
