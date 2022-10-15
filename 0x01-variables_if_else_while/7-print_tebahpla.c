cad4099118f1f1c84c8302233366c3f6cd7e7b5c
Adej0la <recursr.dev@gmail.com>
2022-10-15 21:37:04 +0000

    Add program that prints the alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define LOWER 97 /* Lower limit of alphabet */
+#define UPPER 122 /* Upper limit of alphabet */
+/**
+  * main - Print the lowercase alphabet in reverse
+  *
+  * Description: Print the lowercase alphabet in reverse,
+  * followed by a new line
+  *
+  * Return: Always 0 (Success)
+  */
+int main (void)
+{
+	int i;
+
+	for (i = UPPER; i >= LOWER; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
