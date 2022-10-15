43a5db5959c2a714d0233b5c15a910f6a2e34e5c
Adej0la <recursr.dev@gmail.com>
2022-10-15 21:06:13 +0000

    Add program tha prints all alphabet letters bar q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the alphabet in lowercase
+  *
+  * Description: Print the letters of the alphabet in lowercase,
+  * followed by a new line
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i == 101 || i == 113)
+		{
+			i++;
+		}
+
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
