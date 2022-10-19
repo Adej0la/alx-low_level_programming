8100b7ab96413be6fb56eb522933af933fde8cbc
Adej0la <recursr.dev@gmail.com>
2022-10-19 14:40:32 +0000

    Add a program that prints 'putchar' followed by new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point into program
+ * Return: 0
+ */
+int main(void)
+{
+	print_putchar();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_putchar - Prints the string 'putchar'
+ * Return: void
+ */
+void print_putchar(void)
+{
+	printf("_putchar\n");
+}
