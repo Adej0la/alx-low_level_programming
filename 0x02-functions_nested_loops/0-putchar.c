effc58c121e49bc7bdbb1f69baeec451adec0761
Adej0la <recursr.dev@gmail.com>
2022-10-19 15:57:15 +0000

    Change function to print based off of an array of characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,15 @@
  */
 int main(void)
 {
-	print_putchar();
+	char txt[8] = "_putchar";
+	int txt_arr_size = sizeof(txt) / sizeof(txt[0]);
+	int i;
+
+	for (i = 0; i < txt_arr_size; i++)
+	{
+		_putchar(txt[i]);
+	}
+
+	_putchar(10);
 	return (0);
 }
