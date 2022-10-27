98c5eb19fb22dad7ace94df92ce02142e9b1413e
Adej0la <recursr.dev@gmail.com>
2022-10-27 02:19:00 +0000

    Add function that reverses string input

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: a character string
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int _strlen;
+	int _str_idx;
+	int _mid_idx;
+	int temp;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	_strlen = i;
+	_mid_idx = _strlen / 2;
+	_str_idx = _strlen - 1;
+
+	while (_str_idx >= _mid_idx)
+	{
+		temp = s[_strlen - 1 - _str_idx];
+		s[_strlen - 1 - _str_idx] = s[_str_idx];
+		s[_str_idx] = temp;
+		_str_idx--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,11 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 void reset_to_98(int *);
 void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *str);
 int _putchar(char);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
