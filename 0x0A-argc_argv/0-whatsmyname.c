223ab68f8d3f3a15ea9707e626cb527ba80fc69c
Adej0la <recursr.dev@gmail.com>
2022-11-11 16:52:43 +0000

    Add function that prints the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints program name followed by newline
+ * @argc: number of arguments
+ * @argv: an array of strings
+ *
+ * Return: Always 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
