0628909fa507cab2e02321f031675aff0cd63cb8
Adej0la <recursr.dev@gmail.com>
2022-11-12 21:18:09 +0000

    Add function that creates a char array initialized with a char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars and initializes it with a
+ * specific character
+ * @size: a non-negative size of the array
+ * @c: character to initialize the array with
+ *
+ * Return: a character array initialized with c
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *char_arr;
+
+	if (size > 0)
+	{
+		char_arr = malloc(sizeof(int) * size);
+		i = 0;
+
+		while (i < size)
+		{
+			if (char_arr == NULL)
+			{
+				return (NULL);
+			}
+			char_arr[i] = c;
+			i++;
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (char_arr);
+}
