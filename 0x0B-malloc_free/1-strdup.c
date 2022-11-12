946de6a43e21932071164a5a4bc1a17e9c112543
Adej0la <recursr.dev@gmail.com>
2022-11-12 22:36:56 +0000

    Add function that returns pointer to new duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strdup - returns pointer to new string duplicate of string argument
+ * @str: string to be duplicated
+ *
+ * Return: copy of str
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, size;
+	char *arrcpy;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (str != NULL)
+	{
+		size = 0;
+		i = 0;
+		while (str[i])
+		{
+			size++;
+			i++;
+		}
+
+		arrcpy = malloc(size * sizeof(char));
+
+		i = 0;
+		while (i < size)
+		{
+			if (arrcpy == NULL)
+			{
+				return (NULL);
+			}
+			*(arrcpy + i) = *(str + i);
+			i++;
+		}
+	}
+	return (arrcpy);
+}
