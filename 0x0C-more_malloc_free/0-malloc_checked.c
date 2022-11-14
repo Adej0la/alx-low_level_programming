705b8335c3df8dcb4c1265a05890876ff2134410
Adej0la <recursr.dev@gmail.com>
2022-11-14 20:48:00 +0000

    Add function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to be allocated
+ *
+ * Return: a pointer to void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
