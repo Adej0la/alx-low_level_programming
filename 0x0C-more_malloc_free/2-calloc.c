d23e3bd5890093bb383392aca5f7997aff0e7a12
Adej0la <recursr.dev@gmail.com>
2022-11-15 07:37:57 +0000

    Add function that allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: the number of array elements
+ * size: the size of the array elements
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	char *aux;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(size * nmemb);
+	aux = arr;
+
+	if (arr == NULL)
+		return (NULL);
+	
+	for (i = 0; i < nmemb; i++)
+		aux[i] = 0;
+
+	return (arr);
+}
