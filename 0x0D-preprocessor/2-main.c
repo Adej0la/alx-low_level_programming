81abd0e4422e236c07d771bcc5cac6bf5b23b356
Adej0la <recursr.dev@gmail.com>
2022-11-16 06:16:04 +0000

    Use printf to print expansio of __FILE__ to stdout

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,4 @@
-#include <unistd.h>
 #include <stdio.h>
-
-/**
- * _putchar - prints a character to standard output
- * @c: a character
- *
- * Return: 1 on success
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * main - prints the base file
@@ -19,15 +7,6 @@ int _putchar(char c)
  */
 int main(void)
 {
-	int i;
-
-	i = 0;
-	while (__FILE__[i])
-	{
-		_putchar(__FILE__[i]);
-		i++;
-	}
-	_putchar('\n');
-
+		printf("%s\n", __FILE__);
 	return (0);
 }
