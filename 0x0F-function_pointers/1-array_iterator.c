56dfdc6d6a89d7deae8793bfb8097d36430027b3
Adej0la <recursr.dev@gmail.com>
2022-11-16 22:18:40 +0000

    Add function that execs function on each el of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate over an array
+ * @array: an array items
+ * @size: size of array to be iterated
+ * @action: action to be taken on array items
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (action != NULL)
+			action(array[i]);
+	}
+}
