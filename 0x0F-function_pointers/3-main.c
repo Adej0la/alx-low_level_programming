e647b2cb3d4485070715cbab00b77d5b9d979050
Adej0la <recursr.dev@gmail.com>
2022-11-17 23:05:42 +0000

    Compare atoi val of third argument to int 0 instead of its dereference

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int ac, char *av[])
 	{
 		if (get_op_func(av[2]))
 		{
-			if ((*av[2] != '/' || *av[2] != '%') && *av[3] != 0)
+			if ((*av[2] != '/' || *av[2] != '%') && atoi(av[3]) != 0)
 				printf("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
 			else
 			{
