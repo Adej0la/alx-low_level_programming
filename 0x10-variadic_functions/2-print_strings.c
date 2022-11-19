240e5e4f3943f111582d8ff9990ae026acbbbdf9
Adej0la <recursr.dev@gmail.com>
2022-11-19 08:05:04 +0000

    Add function documentation and refactor code syntax

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,12 @@
 #include "variadic_functions.h"
 
 /**
-*/
+ * print_strings - print strings with a new line
+ * @separator: this separates the string arguments
+ * @n: the number of string arguments
+ *
+ * Return: Nothing
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	typedef unsigned int uint;
@@ -20,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", c_str);
 
-		if ( i != (n - 1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
