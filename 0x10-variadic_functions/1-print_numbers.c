d0dc15b8e8229e91e6cfe2176978314f62c885b6
Adej0la <recursr.dev@gmail.com>
2022-11-19 04:44:24 +0000

    Add function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers followed by a new line
+ * @separator: string to be printed between the numbers
+ * @n: the number of numbers
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	typedef unsigned int uint;
+	uint i;
+	va_list nb;
+
+	va_start(nb, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nb, int));
+		if ((i < n - 1))
+		{
+			if (separator != NULL)
+			{
+				printf("%c ", *separator);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+	}
+
+	printf("\n");
+}
