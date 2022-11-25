20469ff148a128e4d8c1bffdc5ac0374ccf85ebb
Adej0la <recursr.dev@gmail.com>
2022-11-25 05:34:27 +0000

    Add function that executes before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * premain - executes and prints to stdout before main function
+ *
+ * Return: Nothing
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
