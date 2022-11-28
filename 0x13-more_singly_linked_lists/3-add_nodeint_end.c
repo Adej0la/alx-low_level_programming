af857f44661f665b27bc3ed5b816cb5b0c343f56
Adej0la <recursr.dev@gmail.com>
2022-11-28 16:19:55 +0000

    Add function that adds new node at end of linked list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of linked list
+ * @head: a pointer to the head node
+ * @n: a member of the listint_t structure
+ *
+ * Return: the address of the new element
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *node = malloc(sizeof(listint_t)), *lastNode;
+
+	node->n = n;
+	node->next = NULL;
+
+	if (*head == NULL)
+		*head = node;
+
+	else
+	{
+		lastNode = *head;
+
+		while (lastNode->next != NULL)
+			lastNode = lastNode->next;
+
+		lastNode->next = node;
+	}
+
+	return (node);
+}
